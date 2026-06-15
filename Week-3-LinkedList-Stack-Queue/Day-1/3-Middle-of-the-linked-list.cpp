f32493e31c342e9975d5529cbfbebdf95b049ab7
Udit Kumar <uditx1@gmail.com>
2026-06-15 20:29:18 +0530

    Add Day 1 linked list solutions

---
+++ Week-3-LinkedList-Stack-Queue/Day-1/3-Middle-of-the-linked-list.cpp
@@ -0,0 +1,30 @@
+#include<cstddef>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* s = head;
+        ListNode* f = head;
+        while (f != nullptr && f->next != nullptr) {
+            s = s->next;
+            f = f->next->next;
+        }
+        return s;
+    }
+};
