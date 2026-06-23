9fbb79a68c72d478f404d5d5085082c5da8d3021
Udit Kumar <uditx1@gmail.com>
2026-06-23 20:11:12 +0530

    Add Week 4 Day 2 tree problems

---
+++ Week-4-Trees/Day-2/1-Binary-Tree-Inorder-Traversal.cpp
@@ -0,0 +1,42 @@
+#include<vector>
+#include <algorithm>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    void in(TreeNode* root,vector<int> &ans){
+        if(root==nullptr)
+        return ;
+
+        in(root->left,ans);
+        ans.push_back(root->val);
+        in(root->right,ans);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        in(root,ans);
+        return ans;
+    }
+};
